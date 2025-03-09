dc3fc6af0ff9bbb54340a3ba7d174a26f3f8a831
cyril4410 <cyrilpthomas06@gmail.com>
2025-03-09 18:51:44 +0530

    alphabetgame
    starting

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,6 +8,7 @@ import "../Style/dashboard.css";
 import StressChecker from "../Component/Stresschecker";
 import StressCheck from "../Component/StressCheck";
 import SpeechGame from "../Component/SpeechGame";
+import AlphabetGame from "./AlphabetGame";
 
 function Dashboard() {
 //   const [user, setUser] = useState(null);
@@ -58,7 +59,7 @@ function Dashboard() {
             <Route path="Analyser" element={<StressChecker/>}/>
             <Route path="StressCheck" element={<StressCheck/>}/>
             <Route  path="game" element={<SpeechGame/>}/>
-           
+            <Route path="game" element={<AlphabetGame />} />
           </Routes>
 
         </div>
